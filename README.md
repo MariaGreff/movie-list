bc92ad3cf0c38fcc3016d14a8675f26db774fbbc
Maria Kuzmicheva <kuzmichevamariia@gmail.com>
2020-07-29 17:31:19 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+This project was created for the Hackday at </salt>.
+
---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log('App is listening on port ' + port);
